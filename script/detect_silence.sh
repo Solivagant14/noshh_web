519b9f3bfde24d8fc112de41176ea3bdf21b09b0
Solivagant14 <nishantjha360@gmail.com>
2022-06-10 18:49:46 +0530

    update silence detect script

--- script/detect_silence.sh
+++ script/detect_silence.sh
@@ -1,3 +1,3 @@
 IN=$1
 
-ffmpeg -hide_banner -vn -i $IN -af "silencedetect=n=-35dB:d=0.1" -f null - 2>&1 | grep "silencedetect" | awk '{print $NF}'
+ffmpeg -hide_banner -vn -i $IN -af "silencedetect=n=-35dB:d=0.2" -f null - 2>&1 | grep "silencedetect" | awk '{print $NF}'
